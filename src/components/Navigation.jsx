45eed107ba5b84639864b132207a4772f6d3a054
nicolas <menthuthuyoupi.nmp@gmail.com>
2024-02-18 22:11:31 -0300

    corrigiendo color de letras

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -11,15 +11,15 @@ import React from 'react'
 const Navigation = () => {
   return (
     <Navbar className='justify-content-space-between' style={{backgroundColor:'#C70039',width:'100%'}}>
-        <Container className='justify-content-start'>
+        <Container className='justify-content-start text-white'>
             <FontAwesomeIcon icon={faHouse}/>
             <Link to='/' className="text-white ms-1 me-4 text-decoration-none">Home</Link>
 
             <FontAwesomeIcon icon={faBook}/>
             <Link to='contact' className="text-white ms-1 text-decoration-none">Contacto</Link>
         </Container>
-        <Container className='justify-content-end pe-5'>
-          <Navbar.Brand className='me-1'>Happy Cake</Navbar.Brand>
+        <Container className='justify-content-end pe-5 text-white'>
+          <Navbar.Brand className='me-1 text-white'>Happy Cake</Navbar.Brand>
           <FontAwesomeIcon icon={faCakeCandles}/>
         </Container>
     </Navbar>
--- src/view/HomePage.jsx
+++ src/view/HomePage.jsx
@@ -6,7 +6,8 @@ import Home from '../components/Home'
 const HomePage = () => {
   return (
     <Container style={{width:'80%'}}>
-        <h1>bienvenido a Happy Cake</h1>
+        <h1>bienvenido a <b>Happy Cake</b></h1>
+        <p>El lugar de los pasteles felices</p>  
         <Home />
     </Container>
   )
